94e68a79b2905e2a09058d2192dc825e430e55d2
Vishwas Bhaktavatsala <vishwasb@bu.edu>
2023-10-18 11:18:33 -0400

    Initial assignment4 submit

--- assignment4/startup.sh
+++ assignment4/startup.sh
@@ -1,14 +1,20 @@
 #! /bin/bash
 sudo apt update
 sudo apt -y upgrade
-sudo apt-get install python3-pip
-sudo apt-get install git
-cd ~
+sudo apt-get install python3-pip -y
+sudo apt-get install git -y
+sudo apt-get install python3-venv -y
+cd /home/vishwasb
 rm -r DS561-vishwas-assignments
 git clone https://github.com/vishwas21/DS561-vishwas-assignments.git
 cd DS561-vishwas-assignments
-cd assignment4
+pwd
+cp -r /home/vishwasb/DS561-vishwas-assignments/assignment4 /home/vishwasb
+cd /home/vishwasb/assignment4
+python3 -m venv env
+source /home/vishwasb/assignment4/env/bin/activate
 pip3 install -r requirements.txt
+deactivate
 touch assignment4.service
 echo "[Unit]
 Description=Gunicorn instance to serve Assignment 4 app
@@ -28,4 +34,6 @@ sudo cp assignment4.service /etc/systemd/system/assignment4.service
 sudo systemctl stop assignment4
 sudo systemctl daemon-reload
 sudo systemctl start assignment4
+systemctl daemon-reload
+sudo systemctl restart assignment4
 sudo systemctl enable assignment4
